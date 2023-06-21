50529608104c1c2328983054fea14e488fa4dec3
mistereskere <mistereskere@protonmail.com>
2023-06-21 20:47:51 +0200

    aggiunto login e signin

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from connect import connect_to_database, close_database_connection, execute_query
 
 app = Flask(__name__)
 connection = None
 
-@app.route('/users', methods=['GET'])
-def get_all_users():
+@app.route('/login', methods=['POST'])
+def login():
     '''
-    Restituisce tutti gli utenti presenti nel database.
-    :return: lista di utenti
+    Esegue il login.
+    :return: esito del login
     '''
-    return jsonify(execute_query(connection, "SELECT * FROM utente"))
+    username = request.form['username']
+    password = request.form['password']
+    result = execute_query(connection, "SELECT * FROM utente WHERE username = '" + username + "' AND password = '" + password + "'")
+    if result:
+        response = make_response('Login successful', 200)
+    else:
+        response = make_response('Invalid username or password', 401)
+    return response
 
-
+@app.route('/signin', methods=['POST'])
+def signin():
+    '''
+    Esegue la registrazione.
+    :return: esito della registrazione
+    '''
+    username = request.form['username']
+    password = request.form['password']
+    result = execute_query(connection, "SELECT * FROM utente WHERE username = '" + username + "'")
+    if result:
+        response = make_response('Username already exists', 409)
+    else:
+        execute_query(connection, "INSERT INTO utente (username, password) VALUES ('" + username + "', '" + password + "')")
+        response = make_response('Signin successful', 201)
+    return response
 
 if __name__ == '__main__':
     connection = connect_to_database()  # Chiama la funzione per connettersi al database
