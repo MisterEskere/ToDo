24b35cab8e5d6e41b1f7800a7f6f8db3c876a746
mistereskere <mistereskere@protonmail.com>
2023-06-21 21:41:00 +0200

    aggiunte funzioni per utente

--- main.py
+++ main.py
@@ -35,6 +35,39 @@ def signin():
         response = make_response('Signin successful', 201)
     return response
 
+@app.route('/changepass', methods=['POST'])
+def change_password():
+    '''
+    Cambia la password.
+    :return: esito del cambio della password
+    '''
+    username = request.form['username']
+    old_password = request.form['old_password']
+    new_password = request.form['new_password']
+    result = execute_query(connection, "SELECT * FROM utente WHERE username = '" + username + "' AND password = '" + old_password + "'")
+    if result:
+        execute_query(connection, "UPDATE utente SET password = '" + new_password + "' WHERE username = '" + username + "'")
+        response = make_response('Password changed successfully', 200)
+    else:
+        response = make_response('Invalid username or password', 401)
+    return response
+
+@app.route('/deleteuser', methods=['POST'])
+def delete_user():
+    '''
+    Elimina l'utente.
+    :return: esito dell'eliminazione dell'utente
+    '''
+    username = request.form['username']
+    password = request.form['password']
+    result = execute_query(connection, "SELECT * FROM utente WHERE username = '" + username + "' AND password = '" + password + "'")
+    if result:
+        execute_query(connection, "DELETE FROM utente INNER JOIN task ON utente.id = task.id_utente WHERE username = '" + username + "'")
+        response = make_response('User deleted successfully', 200)
+    else:
+        response = make_response('Invalid username or password', 401)
+    return response
+
 if __name__ == '__main__':
     connection = connect_to_database()  # Chiama la funzione per connettersi al database
     app.run()  # Avvia il server Flask
